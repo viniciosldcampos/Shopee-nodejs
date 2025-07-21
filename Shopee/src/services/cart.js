e4f6b13ec15af334b6fddc16542c2d419fae18d1
Vinicios Leite de Campos <125104163+viniciosldcampos@users.noreply.github.com>
2025-07-21 12:09:57 -0300

    Estrutura das funcões do cart.js

---
+++ Shopee/src/services/cart.js
@@ -0,0 +1,25 @@
+// Quais as ações do carrinho
+
+
+// CASOS DE USO - Cada uma com uma função:
+
+// Adicionar item do carrinho
+async function addItem(userCard) {
+    
+}
+
+// Deletar item do carrinho
+async function deleteItem(userCard, name) {
+
+}
+
+// Remover item do carrinho - Apenas diminuindo um item
+async function removeItem(userCard, index) {
+
+}
+
+// Calcular o Total do carrinho
+async function calculateTotal(userCard) {
+
+}
+
